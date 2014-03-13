996c0dd517091cb6bf74f16fd4b89fb179e4a577
Tim Moore <tim.moore@lonelyplanet.com.au>
2014-03-14 10:27:55 +1100

    Add support for Firebug profiling.
    When the FIREBUG_PROFILE environment variable is set to a true value,
    starting the timer in performance tests will automatically start the
    Firebug profiler, and stopping the timer will stop the profiler and
    wait for user input before continuing.

--- lib/performance_test/performance_test_steps.rb
+++ lib/performance_test/performance_test_steps.rb
@@ -1,4 +1,5 @@
 When /^I start the timer$/ do
+  page.execute_script("console.profile()") if ENV['FIREBUG_PROFILE']
   @start_time = DateTime.now
 end
 
@@ -6,4 +7,13 @@ Then /^I stop the timer$/ do
   end_time = DateTime.now
   elapsed_time = ((end_time - @start_time).to_f * 100000000).to_i
   puts "TIME_TAKEN #{elapsed_time}MS"
+  if ENV['FIREBUG_PROFILE']
+    page.execute_script("console.profileEnd()")
+    ask <<-PROMPT, 0
+
+    The execution was profiled with Firebug.
+    Test execution will pause so you can inspect the results.
+    Press Enter to continue:
+    PROMPT
+  end
 end
