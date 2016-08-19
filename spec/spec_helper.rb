e09efb35d8a4e4ce021150f21190a711cfb94cac
david hodges <david.hodges@lonelyplanet.com>
2016-08-19 15:22:01 +0100

    'should' syntax is evil... but must be tolerated

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,1 +1,11 @@
 require 'performance_test/performance_test_runner'
+# see https://relishapp.com/rspec/rspec-expectations/docs/syntax-configuration
+RSpec.configure do |config|
+  config.expect_with :rspec do |c|
+    c.syntax = [:should, :expect]
+  end
+  config.mock_with :rspec do |c|
+    c.syntax = [:should, :expect]
+  end
+end
+
