0cff668fafab68ad4d81503d339c5266e428f7cd
Mario D'Arco <mario.darco@lonelyplanet.com>
2019-08-21 16:54:21 +0100

    WIP

--- lib/performance_test/parallel.rb
+++ lib/performance_test/parallel.rb
@@ -33,7 +33,7 @@ class Parallel
         @task_number += 1
         puts "Executing: #{command[:cmd]}"
         out = IO.popen(command[:cmd])
-        out.each_line { |line| puts line }
+        # out.each_line { |line| puts line }
         put_w_time "[#{command[:name]}] started; #{@commands.length} jobs left to start."
         command[:start] = Time.now
         command[:out] = out
