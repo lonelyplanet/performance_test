6ed257d25bd62110a928767da2b0bbc5e9c461d7
Mario D'Arco <mario.darco.78@gmail.com>
2020-01-30 16:07:16 +0000

    run Rubocop with autofix and Ruby 2.5 parser

--- lib/performance_test/parallel.rb
+++ lib/performance_test/parallel.rb
@@ -1,7 +1,8 @@
+# frozen_string_literal: true
+
 require 'timeout'
 
 class Parallel
-
   GREEN = "\033[32m"
   RED = "\033[31m"
   YELLOW = "\e[33m"
@@ -16,7 +17,7 @@ class Parallel
 
   def run
     @start = Time.now
-    put_w_time %{Started at #{Time.now.strftime("%H:%M:%S")}}
+    put_w_time %(Started at #{Time.now.strftime('%H:%M:%S')})
 
     start_some_children
     wait_for_tasks_to_finish
@@ -27,55 +28,54 @@ class Parallel
   private
 
   def start_some_children
-    while @running.length < @process_count && @commands.length > 0
+    while @running.length < @process_count && !@commands.empty?
       command = @commands.shift
-      if !command.nil?
-        @task_number += 1
-        puts "Executing: #{command[:cmd]}"
-        out = IO.popen(command[:cmd])
-        put_w_time "[#{command[:name]}] started; #{@commands.length} jobs left to start."
-        command[:start] = Time.now
-        command[:out] = out
-        @running[out.pid] = command
-      end
+      next if command.nil?
+
+      @task_number += 1
+      puts "Executing: #{command[:cmd]}"
+      out = IO.popen(command[:cmd])
+      put_w_time "[#{command[:name]}] started; #{@commands.length} jobs left to start."
+      command[:start] = Time.now
+      command[:out] = out
+      @running[out.pid] = command
     end
   end
 
   def wait_for_tasks_to_finish
-    begin
-      while true do # When there are no more child processes wait2 raises Errno::ECHILD
-        pid, status = Process.wait2
-        command = @running.delete(pid)
-        next if command.nil?
-        command[:output] = command[:out].read
-        command[:exitstatus] = status.exitstatus
-        if status.success?
-          put_w_time "#{GREEN}[#{command[:name]}] finished. Elapsed time was #{distance_of_time_to_now(command[:start])}.#{RESET}"
-        else
-          put_w_time "#{RED}[#{command[:name]}] failed. Elapsed time was #{distance_of_time_to_now(command[:start])}.#{RESET}\n\n"
-        end
-        puts_still_running
-        start_some_children
+    loop do # When there are no more child processes wait2 raises Errno::ECHILD
+      pid, status = Process.wait2
+      command = @running.delete(pid)
+      next if command.nil?
+
+      command[:output] = command[:out].read
+      command[:exitstatus] = status.exitstatus
+      if status.success?
+        put_w_time "#{GREEN}[#{command[:name]}] finished. Elapsed time was #{distance_of_time_to_now(command[:start])}.#{RESET}"
+      else
+        put_w_time "#{RED}[#{command[:name]}] failed. Elapsed time was #{distance_of_time_to_now(command[:start])}.#{RESET}\n\n"
       end
-    rescue Errno::ECHILD
-      # Errno::ECHILD indicates you have no child process to wait on.
+      puts_still_running
+      start_some_children
     end
+  rescue Errno::ECHILD
+    # Errno::ECHILD indicates you have no child process to wait on.
   end
 
   def puts_still_running
-    return if @running.length == 0
-    put_w_time "#{YELLOW}Still running: #{@running.values.collect{|v|v[:name]}.join(' ')}#{RESET}"
+    return if @running.empty?
+
+    put_w_time "#{YELLOW}Still running: #{@running.values.collect { |v| v[:name] }.join(' ')}#{RESET}"
   end
 
   def distance_of_time_to_now(time)
     seconds = Time.now - time
     total_minutes = (seconds / 60).floor
     seconds_in_last_minute = (seconds - total_minutes * 60).floor
-    "%02dm %02ds" % [total_minutes, seconds_in_last_minute]
+    format('%02dm %02ds', total_minutes, seconds_in_last_minute)
   end
 
   def put_w_time(thing)
-    puts %{[#{distance_of_time_to_now(@start)}] #{thing}}
+    puts %([#{distance_of_time_to_now(@start)}] #{thing})
   end
-
 end
--- lib/tasks/performance_test.rake
+++ lib/tasks/performance_test.rake
@@ -1,11 +1,13 @@
+# frozen_string_literal: true
+
 require 'performance_test/performance_test_runner'
 
-desc "Run all performance tests against release"
+desc 'Run all performance tests against release'
 task :run_performance_tests_release do
   PerformanceTestRunner.new('release').run
 end
 
-desc "Run all performance tests against beta"
+desc 'Run all performance tests against beta'
 task :run_performance_tests_beta do
   PerformanceTestRunner.new('beta').run
 end
